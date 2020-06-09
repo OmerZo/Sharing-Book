8a48d7acff7965e8154f7175cd182fe01aafbe9e
liat levi <liiatlevi@gmail.com>
2020-06-09 19:19:18 +0300

    I am the champions

--- src/SharingBook/Main.java
+++ src/SharingBook/Main.java
@@ -1,10 +1,55 @@
 package SharingBook;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
+		int numin, idnew, index=0, g=0;
+		String Password, UserName, SecondPassword;
 		
-		System.out.println("Hello world!!");
+		User login;
+		ArrayList<String> wishList;
+		ArrayList<String> ToBorrowList;
+		
+		System.out.println("Welcome to Sharing Book");
+		System.out.println("If you want to sign in enter 1 to log in enter 2 ");
+		Scanner s = new Scanner(System.in);
+		numin = s.nextInt();
+		
+		while ((numin==1)&&(index==0))
+		{
+			if(g==1)
+			{
+				System.out.println("Wrong password enter details again");
+
+			}
+			System.out.println("Enter Id");
+			idnew=s.nextInt();
+			System.out.println("Enter UserName");
+			UserName = s.next();
+			System.out.println("Enter Password");
+			Password = s.next();
+			System.out.println("repeat the password");
+			SecondPassword=s.next();
+			if (Password.equals(SecondPassword)) {
+				index=1;
+			}
+			else
+				g=1;
+			
+			
+			//login.Add_User(wishList, ToBorrowList, UserName, Password, id);
+
+
+			
+			
+
+
+		}
+		
+		
+		
 		Book b1 = new Book("a", "b", 1);
 		System.out.println("Book name = " + b1.getmName());
 	}
--- src/SharingBook/User.java
+++ src/SharingBook/User.java
@@ -1,30 +1,62 @@
 package SharingBook;
+import java.util.ArrayList;
 
 public class User {
-	private Book [] wishList;
-	private Book [] ToBorrowList;
+	private ArrayList<Book> wishList = new ArrayList<Book>();
+	private ArrayList<Book> ToBorrowList = new ArrayList<Book>(); 
+
 	private String UserName;
 	private String Password;
 	private int id;
+	
 	private int Count_of_borrow;
 	private int Count_of_take_book;	
 	
-	public Book[] getWishList() {
+	public int getCount_of_borrow() {
+		return Count_of_borrow;
+	}
+
+	public int getCount_of_take_book() {
+		return Count_of_take_book;
+	}
+
+	public void Add_User(ArrayList<Book> wishList,ArrayList<Book> ToBorrowList,String UserName,String Password,int id )
+	{
+		this.id = id;
+		this.wishList = wishList;
+		this.ToBorrowList= ToBorrowList;
+		this.UserName=UserName;
+		this.Password= Password;
+		this.id=id;
+		this.Count_of_borrow=0;
+		this.Count_of_take_book=0;
+		
+
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ArrayList<Book> getWishList() {
 		return wishList;
 	}
-	public void setWishList(Book[] wishList) {
+	public void setWishList(ArrayList<Book> wishList) {
 		this.wishList = wishList;
 	}
-	public Book[] getToBorrowList() {
+	public ArrayList<Book> getToBorrowList() {
 		return ToBorrowList;
 	}
-	public void setToBorrowList(Book[] toBorrowList) {
+	public void setToBorrowList(ArrayList<Book> toBorrowList) {
 		ToBorrowList = toBorrowList;
 	}
-	public User(Book[] wishList, Book[] toBorrowList) {
+	public User(ArrayList<Book> wishList, ArrayList<Book> toBorrowList) {
 		super();
 		this.wishList = wishList;
-		ToBorrowList = toBorrowList;
+		this.ToBorrowList = toBorrowList;
 	}
 	public String getUserName() {
 		return UserName;
@@ -36,7 +68,7 @@ public class User {
 		return Password;
 	}
 	public void setPassword(String password) {
-		Password = password;
+		this.Password = password;
 	}
 	
 
