31b399fbc7bb6f66efddbfb36325fdba06af5dfe
OmerZo <zohky0528@gmail.com>
2020-06-19 16:20:53 +0300

    Save users

--- src/SharingBook/Main.java
+++ src/SharingBook/Main.java
@@ -1,120 +1,123 @@
 package SharingBook;
+
 import java.util.Scanner;
+import java.util.Set;
 import java.util.ArrayList;
 
-
-
-
 public class Main {
-	
-public static void add_detail_wishlist(ArrayList<Book> wishList)
-{
-	String mName,mAuthor, ifitis;
-	System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
-    Scanner l = new Scanner(System.in);
-    ifitis = l.next();
-    while (ifitis.equals("2"))
-    {
-    	System.out.println("Enter name of book");
-    	mName = l.next();
-    	System.out.println("Enter author of the book");
-    	mAuthor = l.next();
-    	System.out.println("Enter the type of the book");
-		Scanner s = new Scanner(System.in);
-		int mType = s.nextInt();	
-		Book NewBook = new Book(mName,mAuthor,mType);
-		wishList.add(NewBook);
-    	System.out.println("The book is insert");
-    	System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
-    	ifitis = l.next();
 
-    }
-    
+//	public static ArrayList<Book> wishList;
+//	public static ArrayList<Book> ToBorrowList;
+	public static Set<User> usersSet;
 
-	
-}
+	public static void add_detail_wishlist(ArrayList<Book> wishList) {
+		String mName, mAuthor, ifitis;
+		System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
+		Scanner l = new Scanner(System.in);
+		ifitis = l.next();
+		while (ifitis.equals("2")) {
+			System.out.println("Enter name of book");
+			mName = l.next();
+			System.out.println("Enter author of the book");
+			mAuthor = l.next();
+			System.out.println("Enter the type of the book");
+			Scanner s = new Scanner(System.in);
+			int mType = s.nextInt();
+			Book NewBook = new Book(mName, mAuthor, mType);
+			wishList.add(NewBook);
+			System.out.println("The book is insert");
+			System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
+			ifitis = l.next();
 
+		}
 
-public static void add_detail_to_borrowList(ArrayList<Book> ToBorrowList)
-{
-	String mName,mAuthor, ifitis,i;
-	System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
-    Scanner l = new Scanner(System.in);
-    ifitis=l.next();
-    while (ifitis.equals("2"))
-    {
-    	System.out.println("Enter name of book");
-    	mName = l.next();
-    	System.out.println("Enter author of the book");
-    	mAuthor = l.next();
-    	System.out.println("Enter the type of the book");
-		Scanner s = new Scanner(System.in);
-		int mType = l.nextInt();
-		Book NewBook = new Book(mName,mAuthor,mType);
-		ToBorrowList.add(NewBook);
-    	System.out.println("The book is insert");
-    	System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
-        ifitis=l.next();
+	}
 
-    }
-	
-}
+	public static void add_detail_to_borrowList(ArrayList<Book> ToBorrowList) {
+		String mName, mAuthor, ifitis, i;
+		System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
+		Scanner l = new Scanner(System.in);
+		ifitis = l.next();
+		while (ifitis.equals("2")) {
+			System.out.println("Enter name of book");
+			mName = l.next();
+			System.out.println("Enter author of the book");
+			mAuthor = l.next();
+			System.out.println("Enter the type of the book");
+			Scanner s = new Scanner(System.in);
+			int mType = l.nextInt();
+			Book NewBook = new Book(mName, mAuthor, mType);
+			ToBorrowList.add(NewBook);
+			System.out.println("The book is insert");
+			System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
+			ifitis = l.next();
 
+		}
+
+	}
+
+	public static void loadUsers() {
+		FileManager<User> fileM = new FileManager<User>("Users.txt");
+		usersSet = fileM.read();
+
+		for (User user : usersSet) {
+			System.out.println(user);
+		}
+	}
+
+	public static void saveUsers() {
+		FileManager<User> fileM = new FileManager<User>("Users.txt");
+		fileM.write(usersSet);
+	}
 
 	public static void main(String[] args) {
-		int numin, idnew, index=0, g=0;
-		String Password, UserName, SecondPassword, d;
-		
-		ArrayList<Book> wishList= new ArrayList<>();
-		ArrayList<Book> ToBorrowList =  new ArrayList<>();
-		
+		int numin, userId, index = 0, g = 0;
+		String Password, UserName, SecondPassword;
+
+		// ArrayList<Book> wishList= new ArrayList<>();
+		// ArrayList<Book> ToBorrowList = new ArrayList<>();
+		loadUsers();
+
 		System.out.println("Welcome to Sharing Book");
 		System.out.println("If you want to sign in enter 1 to log in enter 2 ");
-		Scanner s = new Scanner(System.in);
-		numin = s.nextInt();
-		
-		while ((numin==1)&&(index==0))
-		{
-			if(g==1)
-			{
+		Scanner scanner = new Scanner(System.in);
+		numin = scanner.nextInt();
+
+		while ((numin == 1) && (index == 0)) {
+			if (g == 1) {
 				System.out.println("Wrong password enter details again");
 
 			}
 			System.out.println("Enter Id");
-			idnew=s.nextInt();
+			userId = scanner.nextInt();
 			System.out.println("Enter UserName");
-			UserName = s.next();
+			UserName = scanner.next();
 			System.out.println("Enter Password");
-			Password = s.next();
+			Password = scanner.next();
 			System.out.println("Repeat the password");
-			SecondPassword=s.next();
+			SecondPassword = scanner.next();
 			if (Password.equals(SecondPassword)) {
-				index=1;
-			}
-			else
-				g=1;
-			
-			add_detail_wishlist(wishList);
-			add_detail_to_borrowList(ToBorrowList);
+				index = 1;
+			} else
+				g = 1;
 
+//			add_detail_wishlist(wishList);
+//			add_detail_to_borrowList(ToBorrowList);
 
-			User NewLogin= new User (wishList,ToBorrowList,UserName,Password,idnew);
-			System.out.println(NewLogin.getId());
+			User newUser = new User(UserName, Password, userId);
+			usersSet.add(newUser);
+			saveUsers();
 			
-            Login log = new Login();
-		    log.createFile();
-		    log.usingBufferedWritter(NewLogin);
-		
+//          Login log = new Login();
+//		    log.createFile();
+//		    log.usingBufferedWritter(newUser);
 
 		}
-		//else
+		// else
 		{
-			
+
 		}
-		
-		
-		
-		
+
 	}
 
 }
--- src/SharingBook/User.java
+++ src/SharingBook/User.java
@@ -1,31 +1,48 @@
 package SharingBook;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+public class User implements Serializable {
 
-public class User {
-	
-	private ArrayList<Book> wishList = new ArrayList<Book>();
-	private ArrayList<Book> ToBorrowList = new ArrayList<Book>(); 
-	
+	private ArrayList<Book> wishList;
+	private ArrayList<Book> toBorrowList;
+
 	private String UserName;
 	private String Password;
 	private int id;
-	
 	private int Count_of_borrow;
-	public ArrayList<Book> getWishList() {
-		return wishList;
+	private int Count_of_take_book;
+
+	public User(String userName, String password, int id) {
+		wishList = new ArrayList<Book>();
+		toBorrowList = new ArrayList<Book>();
+		UserName = userName;
+		Password = password;
+		this.id = id;
 	}
 
-	public void setCount_of_borrow(int count_of_borrow) {
+	public User(ArrayList<Book> wishList, ArrayList<Book> toBorrowList, String userName, String password, int id,
+			int count_of_borrow, int count_of_take_book) {
+		super();
+		this.wishList = wishList;
+		this.toBorrowList = toBorrowList;
+		UserName = userName;
+		Password = password;
+		this.id = id;
 		Count_of_borrow = count_of_borrow;
-	}
-
-	public void setCount_of_take_book(int count_of_take_book) {
 		Count_of_take_book = count_of_take_book;
 	}
-	private int Count_of_take_book;	
-	
+
+	public User(ArrayList<Book> wishList, ArrayList<Book> toBorrowList, String userName, String password, int id) {
+		super();
+		this.wishList = wishList;
+		this.toBorrowList = toBorrowList;
+		UserName = userName;
+		Password = password;
+		this.id = id;
+	}
 
 	public int getCount_of_borrow() {
 		return Count_of_borrow;
@@ -35,127 +52,106 @@ public class User {
 		return Count_of_take_book;
 	}
 
-	public void Add_User(ArrayList<Book> wishList,ArrayList<Book> ToBorrowList,String UserName,String Password,int id )
-	{
+	public void Add_User(ArrayList<Book> wishList, ArrayList<Book> ToBorrowList, String UserName, String Password,
+			int id) {
 		this.id = id;
 		this.wishList = wishList;
-		this.ToBorrowList= ToBorrowList;
-		this.UserName=UserName;
-		this.Password= Password;
-		this.id=id;
-		this.Count_of_borrow=0;
-		this.Count_of_take_book=0;
-		
+		this.toBorrowList = ToBorrowList;
+		this.UserName = UserName;
+		this.Password = Password;
+		this.id = id;
+		this.Count_of_borrow = 0;
+		this.Count_of_take_book = 0;
 
-		
 	}
-	
-	public void Change(User wisher,User want_to_borrow, Book switchBook)
-	
-	{		//need to check if it work
-		
-		if(wisher.getCount_of_borrow()-wisher.getCount_of_take_book()<-3)
-		{
-			System.out.println("you can't borrow until you give to other books ");
-			
-		}
-		else
-		{
-			System.out.println("It is a match ");
-			System.out.println(wisher.wishList);
-			wisher.wishList.remove(switchBook);
-			want_to_borrow.ToBorrowList.remove(switchBook);
-			System.out.println(want_to_borrow.wishList);
-			want_to_borrow.setCount_of_borrow(Count_of_borrow +1);
-			wisher.setCount_of_take_book(Count_of_take_book+1);
-			System.out.println(wisher.wishList);
-			System.out.println(want_to_borrow.wishList);
-			System.out.println("check if it change");
 
-		}
-		
-	}
-	
-	
-	
-	
-	public static void add_detail_to_borrowList(User user,Book book_to_boroow)
-	{
-		
-		user.ToBorrowList.add(book_to_boroow);
-		
-		
-	}
-	public static void add_detail_wish_book(User user,Book wish_book)
-	{
-		
-		user.wishList.add(wish_book);
-		
-		
-	}
-	public static void remove_detail_to_borrowList(User user,Book book_to_boroow)
-	{
-		
-		user.ToBorrowList.remove(book_to_boroow);
-		
-		
-	}
-	public static void remove_detail_to_wishList(User user,Book wishList)
-	{
-		
-		user.ToBorrowList.remove(wishList);
-		
-		
-	}
-	
-	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	
-	public User(ArrayList<Book> wishList,ArrayList<Book> ToBorrowList,String userName, String password, int id) {
-		super();
-		UserName = userName;
-		Password = password;
-		this.id = id;
-		this.ToBorrowList= ToBorrowList;
-		this.wishList=wishList;
-	}
-	
-	
-	
-	
 	public void setWishList(ArrayList<Book> wishList) {
 		this.wishList = wishList;
 	}
-	public  ArrayList<Book> getToBorrowList() {
-		return ToBorrowList;
+
+	public ArrayList<Book> getToBorrowList() {
+		return toBorrowList;
 	}
+
 	public void setToBorrowList(ArrayList<Book> toBorrowList) {
-		ToBorrowList = toBorrowList;
+		this.toBorrowList = toBorrowList;
 	}
-	public User(ArrayList<Book> wishList, ArrayList<Book> toBorrowList) {
-		super();
-		this.wishList = wishList;
-		this.ToBorrowList = toBorrowList;
-	}
+
 	public String getUserName() {
 		return UserName;
 	}
+
 	public void setUserName(String userName) {
 		UserName = userName;
 	}
+
 	public String getPassword() {
 		return Password;
 	}
+
 	public void setPassword(String password) {
 		this.Password = password;
 	}
 
-	
+	public void add_detail_wishlist() {
+		String mName, mAuthor, ifitis;
+		System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
+		Scanner l = new Scanner(System.in);
+		ifitis = l.next();
+		while (ifitis.equals("2")) {
+			System.out.println("Enter name of book");
+			mName = l.next();
+			System.out.println("Enter author of the book");
+			mAuthor = l.next();
+			System.out.println("Enter the type of the book");
+			Scanner s = new Scanner(System.in);
+			int mType = s.nextInt();
+			Book NewBook = new Book(mName, mAuthor, mType);
+			wishList.add(NewBook);
+			System.out.println("The book is insert");
+			System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
+			ifitis = l.next();
 
+		}
+
+	}
+
+	public void add_detail_to_borrowList() {
+		String mName, mAuthor, ifitis, i;
+		System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
+		Scanner scanner = new Scanner(System.in);
+		ifitis = scanner.next();
+		while (ifitis.equals("2")) {
+			System.out.println("Enter name of book");
+			mName = scanner.next();
+			System.out.println("Enter author of the book");
+			mAuthor = scanner.next();
+			System.out.println("Enter the type of the book");
+			Scanner s = new Scanner(System.in);
+			int mType = scanner.nextInt();
+			Book NewBook = new Book(mName, mAuthor, mType);
+			toBorrowList.add(NewBook);
+			System.out.println("The book is insert");
+			System.out.println("Enter your wish 1 to stop enter 2 to enter new book");
+			ifitis = scanner.next();
+
+		}
+
+	}
+
+	
+	@Override
+	public String toString() {
+		return "User [wishList=" + wishList + ", toBorrowList=" + toBorrowList + ", UserName=" + UserName
+				+ ", Password=" + Password + ", id=" + id + ", Count_of_borrow=" + Count_of_borrow
+				+ ", Count_of_take_book=" + Count_of_take_book + "]";
+	}
 }
